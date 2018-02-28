2830b52d4d03fd36e0a8dcdcb483089b2740da95
于阔源 <yukuoyuan@hotmail.com>
2018-02-28 10:13:58 +0800

    提交了主界面的修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,15 @@ const app = getApp()
 var Bmob = require('../../utils/bmob.js');
 Page({
   data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000,
     motto: 'Hello World',
     array: [{
       message: 'foo',
